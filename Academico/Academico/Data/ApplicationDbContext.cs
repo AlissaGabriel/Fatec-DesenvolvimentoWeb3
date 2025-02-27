9a8afcea9d5709756cfb600950e69a77e5635978
AlissaGabriel <alissagabriel14@gmail.com>
2025-02-27 08:55:45 -0300

    first commit

---
+++ Academico/Academico/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
+{
+    //a classe ApplicationDbContext herda de DbContext, 
+    //que é a classe base do Entity Framework Core para interação com o banco de dados.
+    public class ApplicationDbContext:DbContext
+    {
+        //o construtor contém configurações necessárias para conectar ao banco de dados.
+        //o parâmetro 'options' é passado para o construtor da classe base (DbContext).
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+    }
+}
---
+++ Academico/Academico/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace Academico.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public int? Ra { get; set; }
+        public int? UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
